0b13a3f35ab9197dbdf6d308f8c6fc8c46b4093d
Java Nibble <javanibble@gmail.com>
2020-07-05 15:38:48 +0200

    Add Simple Example

---
+++ basic/simple/src/main/java/com/code2bits/maven/examples/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.code2bits.maven.examples;
+
+public class HelloWorld {
+
+    public static void main( String[] args ) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World!");
+        } else {
+            for (String arg : args) {
+                System.out.println("Hello " + arg + "!");
+            }
+        }
+    }
+
+}
