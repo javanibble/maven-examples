6553c9cf63a6b7dbc5a66db3fd96ce97596ba70d
Java Nibble <javanibble@gmail.com>
2020-07-05 12:53:00 +0200

    Add Multi-module Basic example

---
+++ multi-module/multi-module-basic/module1/src/main/java/com/code2bits/maven/examples/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.code2bits.maven.examples;
+
+public class HelloWorld {
+
+    public static void main( String[] args ) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.print("Hello ");
+            for (String arg : args) {
+                System.out.print(arg + "! \n");
+            }
+        }
+    }
+
+}
